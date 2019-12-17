2781796212a4da04dca3a545cceb20fb34684a83
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-12-17 15:51:29 +0300

    Acceptance in progress

--- PolkaTest/Sr25519.cs
+++ PolkaTest/Sr25519.cs
@@ -21,11 +21,13 @@ namespace PolkaTest
         [Fact]
         public void Ok()
         {
+            const payloadSize = 20;
+
             // Version 0.1.1
-            var signaturePayloadBytes = new byte[Consts.MAX_METHOD_BYTES_SZ];
+            var signaturePayloadBytes = new byte[payloadSize];
             var rnd = new Random();
             rnd.NextBytes(signaturePayloadBytes);
-            long payloadLength = Consts.MAX_METHOD_BYTES_SZ;
+            long payloadLength = payloadSize;
 
             byte[] signerPublicKey = Converters.StringToByteArray("0xd678b3e00c4238888bbf08dbbe1d7de77c3f1ca1fc71a5a283770f06f7cd1205");
             byte[] secretKeyVec = Converters.StringToByteArray("0xa81056d713af1ff17b599e60d287952e89301b5208324a0529b62dc7369c745defc9c8dd67b7c59b201bc164163a8978d40010c22743db142a47f2e064480d4b");
@@ -33,13 +35,13 @@ namespace PolkaTest
             var message = signaturePayloadBytes.AsMemory().Slice(0, (int)payloadLength).ToArray();
             var sig2 = Sr25519v011.SignSimple(signerPublicKey, secretKeyVec, message);
 
-            Console.WriteLine("Message: 0x");
-            for (var i=0; i<Consts.MAX_METHOD_BYTES_SZ; i++) {
+            Console.Write("Message: 0x");
+            for (var i=0; i<payloadSize; i++) {
                 Console.Write($"{signaturePayloadBytes[i]:X2}");
             }
             Console.WriteLine("");
 
-            Console.WriteLine("Signature: 0x");
+            Console.Write("Signature: 0x");
             for (var i=0; i<sig2.Length; i++) {
                 Console.Write($"{sig2[i]:X2}");
             }
