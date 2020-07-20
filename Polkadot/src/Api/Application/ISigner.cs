c700520ff873069345d722eb1febd122506b47ab
sotmorskiy <sotmorskiy@usetech.ru>
2020-07-20 11:19:07 +0700

    Fixed .gitignore.

---
+++ Polkadot/src/Api/Application/ISigner.cs
@@ -0,0 +1,17 @@
+﻿using Polkadot.BinaryContracts;
+
+namespace Polkadot.Api
+{
+    public interface ISigner
+    {
+        IExtrinsicSignature Sign(byte[] publicKey, byte[] privateKey, byte[] message);
+
+        bool VerifySignature(byte[] sign, byte[] publicKey, byte[] message);
+
+        byte[] GetSignaturePayload<TAddress, TSignature, TSignedExtra, TCall>(UncheckedExtrinsic<TAddress, TSignature, TSignedExtra, TCall> extrinsic)
+            where TAddress : IExtrinsicAddress
+            where TSignature : IExtrinsicSignature
+            where TSignedExtra : IExtrinsicExtra
+            where TCall : IExtrinsicCall;
+    }
+}
