7e6cb9d5cada5a7bc00ae2e5979968281581b0db
sotmorskiy <sotmorskiy@usetech.ru>
2020-11-02 13:36:40 +0700

    Added SCALE codec test.

---
+++ PolkaTest/ScaleTest.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Polkadot.BinarySerializer;
+using Xunit;
+
+namespace PolkaTest
+{
+    public class ScaleTest
+    {
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(62L)]
+        [InlineData(63L)]
+        [InlineData(64L)]
+        [InlineData(65L)]
+        [InlineData(4096L)]
+        [InlineData(16382L)]
+        [InlineData(16383L)]
+        [InlineData(16384L)]
+        [InlineData(1073741822L)]
+        [InlineData(1073741823L)]
+        [InlineData(1073741824L)]
+        public void EncodingAndDecodingNumbersDoesntChangeThem(long number)
+        {
+            var encoded = Scale.EncodeCompactInteger(number);
+            var ms = new MemoryStream(encoded.Bytes);
+            var decoded = Scale.DecodeCompactInteger(ms);
+            Assert.Equal(number, (long)decoded.Value);
+        }
+    }
+}
